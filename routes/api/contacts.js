2260c2238a756541b61ad859062d1d6bd28eff5e
Volodymyr Ogiychuk <109019728+Volodymyr-Ogiychuk@users.noreply.github.com>
2023-01-16 10:32:29 +0200

    Revert "Add completed task"

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const {
-  getContacts,
-  getById,
-  updateContactById,
-  createContact,
-  deleteContactById,
-  setFavorite,
-} = require('../../controllers/contacts-controller');
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+
+} = require("../../models/contacts");
 
 const Joi = require("joi")
 
@@ -16,7 +16,6 @@ const addContactSchema = Joi.object({
     .email({ tlds: { deny: ["ru"] } })
     .required(),
   phone: Joi.string().required(),
-  favorite: Joi.boolean(),
 });
 
 const updateContactSchema = Joi.object({
@@ -24,10 +23,6 @@ const updateContactSchema = Joi.object({
   email: Joi.string().email({ tlds: { deny: ["ru"] } }),
   phone: Joi.string(),
 }).or("name", "email", "phone");
-
-const updateFavoriteSchema = Joi.object({
-  favorete: Joi.boolean().required,
-});
 
 const validator = (schema) => (req, res, next) => {
   const body = req.body;
@@ -43,14 +38,50 @@ const validator = (schema) => (req, res, next) => {
 
 const router = express.Router()
 
-router.get('/', getContacts);
-router.get('/:contactId', getById);
-router.post('/', validator(addContactSchema), createContact);
-router.delete('/:contactId', deleteContactById);
-router.put("/:contactId", validator(updateContactSchema), updateContactById);
-router.patch("/:contactId/favorite",
-  validator(updateFavoriteSchema),
-  setFavorite
-);
+router.get('/', async (req, res, next) => {
+  const contacts = await listContacts();
+  res.set("Content-Type", "application/json").send(contacts);
+})
 
-module.exports = router;
+router.get('/:contactId', async (req, res, next) => {
+  const id = req.params.contactId;
+  const contact = await getContactById(id);
+
+  if (!contact) {
+    res.status(404).json({ message: "Not found" }).end();
+    return;
+  }
+
+  res.json(contact);
+})
+
+router.post('/', async (req, res, next) => {
+  const newContact = await addContact(req.body);
+
+  res.status(201).json(newContact);
+})
+
+router.delete('/:contactId', async (req, res, next) => {
+  const id = req.params.contactId;
+  const isContactExist = await removeContact(id);
+
+  isContactExist
+    ? res.status(200).json({ message: "Contact deleted" })
+    : res.status(404).json({ message: "Not found" });
+})
+
+router.put("/:contactId",
+  validator(updateContactSchema),
+  async (req, res, next) => {
+    const id = req.params.contactId;
+    const contactChanges = req.body;
+    const updatedContact = await updateContact(id, contactChanges);
+
+    updatedContact
+      ? res.status(200).json(updatedContact)
+   
+   : res.status(404).json({ message: "Not found" });
+
+})
+
+module.exports = router
